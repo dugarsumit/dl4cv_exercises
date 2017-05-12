230bb74d57a4f9cee756c70bf8e73f4146671ea3
sumit dugar <dugar.sumit1990@gmail.com>
2017-05-12 17:03:00 +0200

    removed metlabpolis

--- DL4CV_Exercise1/my_file.py
+++ DL4CV_Exercise1/my_file.py
@@ -2,7 +2,4 @@ import numpy as np
 
 a = np.array([[1,2,3],[4,5,6],[7,8,9]])
 b = [2, 1, 2]
-#print(np.hstack((a, b)))
-#print(np.append(a,b,axis=0))
-print(np.column_stack((a,b)))
-print(np.row_stack((a,b)))
+print(np.logspace(-3, -4, 15))
